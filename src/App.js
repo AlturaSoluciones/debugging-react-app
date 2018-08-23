54add7edbaac679795cba496d8945c906d87d42a
Miguel Torres <miguel@alturasoluciones.com>
2018-08-23 12:00:02 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      date: new Date(),
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
